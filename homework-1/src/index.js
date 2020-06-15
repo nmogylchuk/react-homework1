06f17cb8d60ddfbb1f4ddf633d5136ee5d651724
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-06-15 18:26:58 +0300

    Add ordered list of 4-6 presidents with attribute "start"

--- homework-1/src/index.js
+++ homework-1/src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <div>
     <ul>
       <li>George Washington</li>
       <li>John Adams</li>
       <li>Thomas Jefferson</li>
-    </ul>,
+    </ul>
+    <ol start="4">
+      <li>James Madison</li>
+      <li>James Monroe</li>
+      <li>John Quincy Adams</li>
+    </ol>
+  </div>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
