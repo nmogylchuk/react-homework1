bcd8b7afcf713a4fe780b59200edbdef99878be0
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-06-15 22:31:55 +0300

    Add unordered list of 10-14 presidents with even odd (subtask 5)

--- homework-1/src/index.js
+++ homework-1/src/index.js
@@ -9,6 +9,38 @@ const arrayOfPresidentsFrom7To9 = [
   'William Henry Harrison'
 ];
 
+const listOfPresidentsFrom10To14 = [
+  {
+    firstName: 'John',
+    lastName: 'Tyler',
+    presidentIndex: 10
+  },
+  {
+    firstName: 'James K.',
+    lastName: 'Polk',
+    presidentIndex: 11
+  },
+  {
+    firstName: 'Zachary',
+    lastName: 'Taylor',
+    presidentIndex: 12
+  },
+  {
+    firstName: 'Millard',
+    lastName: 'Fillmore',
+    presidentIndex: 13
+  },
+  {
+    firstName: 'Franklin',
+    lastName: 'Pierce',
+    presidentIndex: 14
+  }
+];
+
+const formatedListOfPresidentsFrom10To14 = ({ firstName, lastName, presidentIndex }) => {
+  return `${lastName}, ${firstName}, ${presidentIndex}th`;
+}
+
 ReactDOM.render(
   <div>
     <ul>
@@ -23,7 +55,18 @@ ReactDOM.render(
     </ol>
     <ul>
       {arrayOfPresidentsFrom7To9.map((president) =>
-        <li key={president}>{president}</li>
+        <li key={president}>
+          {president}
+        </li>
+      )}
+    </ul>
+    <ul>
+      {listOfPresidentsFrom10To14.map(president =>
+        president.presidentIndex % 2 === 1 ? (
+          <li key={president.presidentIndex}>
+            {formatedListOfPresidentsFrom10To14(president)}
+          </li>
+        ) : null
       )}
     </ul>
   </div>,
